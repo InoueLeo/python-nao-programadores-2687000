f3582a9abb54fe2de051934b1304cf4217850fd3
InoueLeo <leonardo.inouee@outlook.com>
2025-01-28 02:09:01 +0000

    exerciciovariaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,17 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Leonardo'
+profissão = "Bancário"
+Sinopse = '''testet teste teste teste 
+ teste  teste  teste  teste 
+  teste  teste  teste  teste '''
 
 # Declare variáveis de cada tipo numérico
+idade = 26
+dinheiro = 1548.25
 
 
 # Declare uma variável do tipo booleano
+Maior_idade = True
+Menor_idade = False
